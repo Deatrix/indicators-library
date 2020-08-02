7e8304e6fd3c0df6dcd9579556e52769e87e6029
Deatrix <syedraiyan2013@gmail.com>
2020-08-02 00:30:07 -0400

    Update ind_lib.lua

--- ind_lib.lua
+++ ind_lib.lua
@@ -10,7 +10,7 @@ i = 0
 local ref = gui.Reference("RAGEBOT")
 local tab = gui.Tab(ref, "tab_ind_opt", "Indicators Settings")
 local gb = gui.Groupbox(tab, "Size", 15, 15, 300, 900)
-local gb_1 = gui.Groupbox(tab, "Position", 325, 15, 300, 900)
+local gb_1 = gui.Groupbox(tab, "Position", 300, 15, 300, 900)
 
 local fnt_name = gui.Combobox(gb, "fnt_name", "Font Name", "Tahoma", "Verdana")
 local fnt_size = gui.Slider(gb, "fnt_size", "Font Size", 27, 12, 48)
